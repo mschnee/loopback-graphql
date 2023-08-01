486c6d9ca6e08b6f10747c1c258823be6e6cc056
Matthew Schnee <i.am@matthew.engineer>
2023-08-01 12:02:20 -0700

    fix: make 'prism-react-renderer' an explicit versioned dependency

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -4,6 +4,7 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
+const {themes} = require('prism-react-renderer');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: '@loopback/graphql',
